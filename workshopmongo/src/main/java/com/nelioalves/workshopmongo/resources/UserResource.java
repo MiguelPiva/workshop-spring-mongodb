058081e3c3da16aaf558507eb6627cc5764723f0
Miguel Piva <miguel.piva@outlook.com>
2024-08-17 23:24:39 -0300

    Atualização de usuário
    Atualização de usuário por id utilizando o método PUT

--- workshopmongo/src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ workshopmongo/src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
@@ -52,6 +54,14 @@ public class UserResource {
     @DeleteMapping(value="/{id}")
     public ResponseEntity<UserDTO> delete(@PathVariable String id) {
         service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id) {
+        User obj = service.fromDTO(objDTO);
+        obj.setId(id);
+        obj = service.update(obj);
         return ResponseEntity.noContent().build();
     }
 }
