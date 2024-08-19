7c03550ca853c10d63b2a30dd372b6851cd8fed1
Miguel Piva <miguel.piva@outlook.com>
2024-08-19 00:07:07 -0300

    Referenciando os posts do usuário

--- workshopmongo/src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
+++ workshopmongo/src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
@@ -34,12 +34,15 @@ public class Instantiation implements CommandLineRunner{
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-        
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
         Post post1 = new Post(null, sdf.parse("21/08/2023"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/08/2023"), "Bom dia", "Acordei com vontade de comer pizza e milkshake hoje. Meu nutricionista não pode saber.", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
